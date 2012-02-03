41fcdf81a87c214feb765ba0c34e9fa1b405bd50
Tyler Akins <fidian@rumkin.com>
2012-02-03 09:57:36 -0600

    Allow filename as parameter, changed output format to match tests

--- bin/tokenize.js
+++ bin/tokenize.js
@@ -1,6 +1,28 @@
 #!/usr/bin/node
 var tokenizer = require('../lib/tokenizer');
 var fs = require('fs');
-var contents = fs.readFileSync(__dirname + '/test/css/simple.css', 'utf-8');
-var result = tokenizer.tokenize(contents);
-console.log(result);
+var filenames = process.argv;
+filenames.shift(); // Remove "node"
+filenames.shift(); // Remove current script
+var count = 0;
+
+for (var i = 0; i < filenames.length; i ++) {
+	var contents = fs.readFileSync(filenames[i], 'utf-8');
+	var result = tokenizer.tokenize(contents);
+	process.stdout.write("[\n");
+
+	for (var i = 0; i < result.tokens.length; i ++) {
+		if (i) {
+			process.stdout.write(",\n");
+		}
+
+		var j = JSON.stringify(result.tokens[i]);
+		process.stdout.write("\t" + j);
+	}
+	process.stdout.write("\n]\n");
+	count ++;
+}
+
+if (count == 0) {
+	console.log('Please pass filenames on the command line');
+}
