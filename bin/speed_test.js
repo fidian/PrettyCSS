6e3b59dc3a0c1fed53948768fb9f1fd467cf7b72
Tyler Akins <fidian@rumkin.com>
2012-02-03 09:57:53 -0600

    Specify filename as command-line parameter

--- bin/speed_test.js
+++ bin/speed_test.js
@@ -1,23 +1,32 @@
 #!/usr/bin/node
-var start = new Date();
-var tokenizer = require('../lib/tokenizer');
-var end = new Date();
-var initTime = end - start;
-console.log("init:  " + initTime);
-var fs = require('fs');
-var contents = fs.readFileSync(__dirname + '/../test/css/styles.css', 'utf-8');
+var filename = process.argv;
+filename.shift(); // Remove "node"
+filename.shift(); // Remove current script
+filename = filename.shift();
 
-var total = 0;
-var loops = 10;
-for (var i = 0; i < loops; i ++) {
-	start = new Date();
-	tokenizer.tokenize(contents);
-	end = new Date();
-	var elapsed = end - start;
-	console.log(elapsed);
-	total += elapsed;
+if (! filename) {
+	console.log("Specify a filename on the command line");
+} else {
+	var start = new Date();
+	var tokenizer = require('../lib/tokenizer');
+	var end = new Date();
+	var initTime = end - start;
+	console.log("init:  " + initTime);
+	var fs = require('fs');
+	var contents = fs.readFileSync(filename, 'utf-8');
+
+	var total = 0;
+	var loops = 10;
+	for (var i = 0; i < loops; i ++) {
+		start = new Date();
+		tokenizer.tokenize(contents);
+		end = new Date();
+		var elapsed = end - start;
+		console.log(elapsed);
+		total += elapsed;
+	}
+
+	console.log("TOTAL: " + total);
+	console.log("  AVG: " + Math.round(total / loops));
+	console.log(" INIT: " + initTime);
 }
-
-console.log("TOTAL: " + total);
-console.log("  AVG: " + Math.round(total / loops));
-console.log(" INIT: " + initTime);
