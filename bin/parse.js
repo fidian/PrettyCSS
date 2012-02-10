db311cfe5d746cb7eec36ed74ebea8d792b8d55f
Tyler Akins <fidian@rumkin.com>
2012-02-10 15:19:29 -0600

    Reworked how parsing and converting to a string works
    Liking it now.... Need to add validation

--- bin/parse.js
+++ bin/parse.js
@@ -5,16 +5,55 @@ var filenames = process.argv;
 filenames.shift(); // Remove "node"
 filenames.shift(); // Remove current script
 var count = 0;
+var parseOptions = true;
+var parseFiles = true;
+var options = {
+	debug: false
+};
 
 for (var i = 0; i < filenames.length; i ++) {
-	var contents = fs.readFileSync(filenames[i], 'utf-8');
-	var result = prettycss.parse(contents, {
-		//debug: true
-	});
-	console.log(result.toString());
-	count ++;
+	var parseThisFile = true;
+
+	if (parseOptions) {
+		parseThisFile = false;
+
+		switch (filenames[i]) {
+			case '--debug':
+			case '-d':
+				options.debug = true;
+				break;
+
+			case '--help':
+			case '-h':
+				help();
+				parseOptions = false;
+				parseFiles = false;
+				break;
+
+			case '--':
+				parseOptions = false;
+				break;
+
+			default:
+				parseThisFile = true;
+		}
+	}
+
+	if (parseFiles && parseThisFile) {
+		var contents = fs.readFileSync(filenames[i], 'utf-8');
+		var result = prettycss.parse(contents, options);
+		console.log(result.toString());
+		count ++;
+	}
 }
 
 if (count == 0) {
 	console.log('Please pass filenames on the command line');
+}
+
+function help() {
+	console.log("Parse a CSS file");
+	console.log("Options:");
+	console.log(" -h = Help (what you are reading right now)");
+	console.log(" -d = Debug mode");
 }
