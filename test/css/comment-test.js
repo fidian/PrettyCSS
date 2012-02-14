e08d0205e626ac05ae7f33f596433e7100e15842
Tyler Akins <fidian@rumkin.com>
2012-02-14 17:59:50 -0600

    Added comment tests

---
+++ test/css/comment-test.js
@@ -0,0 +1,9 @@
+var vows = require('vows');
+var comment = require('../../lib/css/comment')
+var util = require('./util');
+
+vows.describe('lib/css/comment.js').addBatch({
+	'comment.css': util.tokenizeFile({
+		'comment.json': util.compareResult(comment)
+	})
+}).export(module);
