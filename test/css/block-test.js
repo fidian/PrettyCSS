3ccec6d8159b640e75db9be4d634c16ee3871197
Tyler Akins <fidian@rumkin.com>
2012-02-14 17:51:30 -0600

    Added tests for block

---
+++ test/css/block-test.js
@@ -0,0 +1,18 @@
+var vows = require('vows');
+var block = require('../../lib/css/block')
+var util = require('./util');
+
+vows.describe('lib/css/block.js').addBatch({
+	'block.css': util.tokenizeFile({
+		'block.json': util.compareResult(block)
+	}),
+	'block-bad.css': util.tokenizeFile({
+		'block-bad.json': util.compareResult(block)
+	}),
+	'block-nested.css': util.tokenizeFile({
+		'block-nested.json': util.compareResult(block)
+	}),
+	'block-nested-bad.css': util.tokenizeFile({
+		'block-nested-bad.json': util.compareResult(block)
+	})
+}).export(module);
