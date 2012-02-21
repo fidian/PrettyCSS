186c7cc1fed14900ebfcbfe110e2bbbd7dd39863
Tyler Akins <fidian@rumkin.com>
2012-02-21 11:56:08 -0600

    Adding extended information to utility

--- bin/parse.js
+++ bin/parse.js
@@ -8,7 +8,8 @@ var count = 0;
 var parseOptions = true;
 var parseFiles = true;
 var options = {
-	debug: false
+	debug: false,
+	extendedInfo: false
 };
 
 for (var i = 0; i < filenames.length; i ++) {
@@ -21,6 +22,11 @@ for (var i = 0; i < filenames.length; i ++) {
 			case '--debug':
 			case '-d':
 				options.debug = true;
+				break;
+
+			case '--extended-info':
+			case '-e':
+				options.extendedInfo = true;
 				break;
 
 			case '--help':
@@ -40,9 +46,66 @@ for (var i = 0; i < filenames.length; i ++) {
 	}
 
 	if (parseFiles && parseThisFile) {
+		var timeStart = new Date();
 		var contents = fs.readFileSync(filenames[i], 'utf-8');
+		var timeLoaded = new Date();
 		var result = prettycss.parse(contents, options);
-		console.log(result.toString());
+		var timeParsed = new Date();
+		var resultString = result.toString();
+		var timeToString = new Date();
+		console.log(resultString);
+
+		if (options.extendedInfo) {
+			var stars = "************************************";
+			var showProblem = function (problemInfo) {
+				var msg = "\t" + problemInfo.code + "  ";
+
+				if (problemInfo.token) {
+					msg += problemInfo.token.type + " (" + problemInfo.token.toString() + ") on line " + problemInfo.token.line;
+				} else {
+					msg += "[no token]";
+				}
+
+				console.log(msg);
+			};
+			var formatTime = function (t) {
+				var ms = t % 1000;
+				var s = Math.floor(t / 1000);
+				ms = ("00" + ms.toString()).substr(-3);
+				return s + "." + ms + " sec";
+			};
+
+			console.log("");
+			console.log("/" + stars + stars);
+			console.log("");
+			console.log("Extended Information:");
+			console.log("\tInput size:  " + contents.length);
+			console.log("\tOutput size:  " + resultString.length);
+			console.log("\tTime to read file:  " + formatTime(timeLoaded - timeStart));
+			console.log("\tTime to tokenize and parse:  " + formatTime(timeParsed - timeLoaded));
+			console.log("\tTime to convert to a string:  " + formatTime(timeToString - timeParsed));
+			console.log("");
+
+			if (result.errors.length) {
+				console.log("Errors:");
+				result.errors.forEach(showProblem);
+			} else {
+				console.log("No errors detected.");
+			}
+
+			console.log("");
+
+			if (result.warnings.length) {
+				console.log("Warnings:");
+				result.warnings.forEach(showProblem);
+			} else {
+				console.log("No warnings detected.");
+			}
+
+			console.log("");
+			console.log(stars + stars + "/");
+		}
+
 		count ++;
 	}
 }
@@ -54,6 +117,7 @@ if (count == 0) {
 function help() {
 	console.log("Parse a CSS file");
 	console.log("Options:");
-	console.log(" -h = Help (what you are reading right now)");
 	console.log(" -d = Debug mode");
+	console.log(" -e = Extended info - appends a comment with errors/warnings");
+	console.log(" -h = Help (what you are reading right now)");
 }
