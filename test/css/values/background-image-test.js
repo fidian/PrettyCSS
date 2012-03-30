0ce20175f1ad2ca833f5af73830696cb68afd4c8
Tyler Akins <fidian@rumkin.com>
2012-03-30 14:26:16 -0500

    New test for background image

---
+++ test/css/values/background-image-test.js
@@ -0,0 +1,30 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('background-image', {
+	// Test bg-layer value 
+	'none url(a)': {
+		'tokens': ['IDENT', 'S', 'URL'],
+		'toString': 'none',
+		'unparsed': ['URL'],
+		'warnings': []
+	},
+	'url(google), url(yahoo)': {
+		'tokens': ['URL', 'OPERATOR', 'S', 'URL'],
+		'toString': 'url(google), url(yahoo)',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_3']
+	},
+	'url(a) cookie monster': {
+		'tokens': ['URL', 'S', 'IDENT', 'S', 'IDENT'],
+		'toString': 'url(a)',
+		'unparsed': ['IDENT', 'S', 'IDENT'],
+		'warnings': []
+	},
+	'alsjdjd': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
