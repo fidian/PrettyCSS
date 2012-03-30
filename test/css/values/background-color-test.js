6e9ea5af9fd68d9b865d88ddd359d673e44efb7b
Tyler Akins <fidian@rumkin.com>
2012-03-30 14:13:28 -0500

    Updating to new style

--- test/css/values/background-color-test.js
+++ test/css/values/background-color-test.js
@@ -1,40 +1,35 @@
 "use strict";
-var vows = require('vows');
 var util = require('./util');
 
-util.name = 'background-color';
-util.obj = require('../../../lib/css/values/' + util.name);
-
-exports.batch = vows.describe('lib/css/' + util.name + '.js').addBatch({
-
-	'#fff': util.testValue({
+exports.batch = util.makeVows('background-color', {
+	'#fff': {
 		'tokens': ['HASH'],
 		'toString': '#fff',
 		'unparsed': [],
 		'warnings': []
-	}),
-	'#fff ajsdjlksdj': util.testValue({
+	},
+	'#fff ajsdjlksdj': {
 		'tokens': ['HASH', 'S', 'IDENT'],
 		'toString': '#fff',
 		'unparsed': ['IDENT'],
 		'warnings': []
-	}),
-	'inherit': util.testValue({
+	},
+	'inherit': {
 		'tokens': ['IDENT'],	
 		'toString': 'inherit',
 		'unparsed': [],
 		'warnings': ['minimum_css_version_2', 'browser_unsupported_IE7', 'browser_quirk_IE8']
-	}),
-	'initial': util.testValue({
+	},
+	'initial': {
 		'tokens': ['IDENT'],		
 		'toString': 'initial',  	
 		'unparsed': [],
 		'warnings': ['minimum_css_version_3']
-	}),		          	
-	'klasdfkljsd': util.testValue({
+	},		          	
+	'klasdfkljsd': {
 		'tokens': ['IDENT'],
 		'toString': null,
 		'unparsed': ['IDENT'],
 		'warnings': []
-	})
+	}
 });
--- test/css/values/background-test.js
+++ test/css/values/background-test.js
@@ -1,34 +1,30 @@
 "use strict";
-var vows = require('vows');
 var util = require('./util');
 
-util.name = 'background';
-util.obj = require('../../../lib/css/values/' + util.name);
-
-exports.batch = vows.describe('lib/css/' + util.name + '.js').addBatch({
+exports.batch = util.makeVows('background', {
 	// Test bg-layer value 
-	'#fff': util.testValue({
+	'#fff': {
 		'tokens': ['HASH'],
 		'toString': '#fff',
 		'unparsed': [],
 		'warnings': []
-	}),
-	'#fff, #000, #f00, blue': util.testValue({
+	},
+	'#fff, #000, #f00, blue': {
 		'tokens': ['HASH', 'OPERATOR', 'S', 'HASH', 'OPERATOR', 'S', 'HASH', 'OPERATOR', 'S', 'IDENT'],
 		'toString': '#fff, #000, #f00, blue',
 		'unparsed': [],
 		'warnings': ['illegal_value', 'illegal_value', 'illegal_value']
-	}),
-	'url(a)': util.testValue({
+	},
+	'url(a)': {
 		'tokens': ['URL'],
 		'toString': 'url(a)',
 		'unparsed': [],
 		'warnings': []
-	}),
-	'url(a) alsjdjd': util.testValue({
+	},
+	'url(a) alsjdjd': {
 		'tokens': ['URL','S', 'IDENT'],
 		'toString': 'url(a)',
 		'unparsed': ['IDENT'],
 		'warnings': []
-	})
+	}
 });
