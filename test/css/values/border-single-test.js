13f87b70c373d2ffd873f40e80f3eca5b69d23d7
Tyler Akins <fidian@rumkin.com>
2012-04-03 15:50:56 -0500

    Adding test and fixing variable name

---
+++ test/css/values/border-single-test.js
@@ -0,0 +1,41 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('border-single', {
+	'5px': {
+		'tokens': ['UNIT'],
+		'toString': '5px',
+		'unparsed': [],
+		'warnings': []
+	},
+	'solid 5px black': {
+		'tokens': ['IDENT', 'S', 'UNIT', 'S', 'IDENT'],
+		'toString': 'solid 5px black',
+		'unparsed': [],
+		'warnings': []
+	},
+	'solid, 5px, black': {
+		'tokens': ['IDENT', 'OPERATOR', 'S', 'UNIT', 'OPERATOR', 'S', 'IDENT'],
+		'toString': 'solid',
+		'unparsed': ['OPERATOR', 'S', 'UNIT', 'OPERATOR', 'S', 'IDENT'],
+		'warnings': []
+	},
+	'solid inherit': {
+		'tokens': ['IDENT', 'S', 'IDENT'],
+		'toString': 'solid inherit',
+		'unparsed': [],
+		'warnings': ['inherit_not_allowed']
+	},
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': 'inherit',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2']
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
