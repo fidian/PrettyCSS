39531f89f45f05f3b9c411ef5995765d6e560184
Tyler Akins <fidian@rumkin.com>
2012-04-03 17:14:07 -0500

    Initial commit

---
+++ test/css/values/counter-test.js
@@ -0,0 +1,42 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('counter', {
+	'counters(mcmacken,"smacky",square)': {
+		'tokens': ['FUNCTION', 'IDENT', 'OPERATOR', 'STRING', 'OPERATOR', 'IDENT', 'PAREN_CLOSE'],
+		'toString': 'counters(mcmacken, "smacky", square)',
+		'unparsed': [],
+		'warnings': []
+	},
+	'counters( inherit , "smacky" )': {
+		'tokens': ['FUNCTION', 'S', 'IDENT', 'S', 'OPERATOR', 'S', 'STRING', 'S', 'PAREN_CLOSE'],
+		'toString': 'counters(inherit, "smacky")',
+		'unparsed': [],
+		'warnings': ['inherit_not_allowed']
+	},
+	'counter(mcmacken,square) balloon': {
+		'tokens': ['FUNCTION', 'IDENT', 'OPERATOR', 'IDENT', 'PAREN_CLOSE', 'S', 'IDENT'],
+		'toString': 'counter(mcmacken, square)',
+		'unparsed': ['IDENT'],
+		'warnings': []
+	},
+	'counter( bubbly )': {
+		'tokens': ['FUNCTION', 'S', 'IDENT', 'S', 'PAREN_CLOSE'],
+		'toString': 'counter(bubbly)',
+		'unparsed': [],
+		'warnings': []
+	},
+	'counter( bubbly': {
+		'tokens': ['FUNCTION', 'S', 'IDENT'],
+		'toString': null,
+		'unparsed': ['FUNCTION', 'S', 'IDENT'],
+		'warnings': null
+	},
+	// Inherit isn't allowed anywhere for this object
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
