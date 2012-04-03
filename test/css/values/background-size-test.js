62b9aca784e82c4d435eb03aac2462c50637856d
Tyler Akins <fidian@rumkin.com>
2012-04-03 12:23:33 -0500

    Initial commit

---
+++ test/css/values/background-size-test.js
@@ -0,0 +1,42 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('background-size', {
+	'cover': {
+		'tokens': ['IDENT'],
+		'toString': 'cover',
+		'unparsed': [],
+		'warnings': []
+	},
+	'cover auto blah': {
+		'tokens': ['IDENT', 'S', 'IDENT', 'S', 'IDENT'],
+		'toString': 'cover',
+		'unparsed': ['IDENT', 'S', 'IDENT'],
+		'warnings': []
+	},
+	'cover, auto, blah': {
+		'tokens': ['IDENT', 'OPERATOR', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT'],
+		'toString': 'cover, auto',
+		'unparsed': ['OPERATOR', 'S', 'IDENT'],
+		'warnings': []
+	},
+	'inherit, cover': {
+		'tokens': ['IDENT', 'OPERATOR', 'S', 'IDENT'],
+		'toString': 'inherit',
+		'unparsed': ['OPERATOR', 'S', 'IDENT'],
+		'warnings': ['minimum_css_version_2']
+	},
+	// bg-size does not match "inherit"
+	'cover, inherit': {
+		'tokens': ['IDENT', 'OPERATOR', 'S', 'IDENT'],
+		'toString': 'cover',
+		'unparsed': ['OPERATOR', 'S', 'IDENT'],
+		'warnings': []
+	},
+	'I dent': {
+		'tokens': ['IDENT', 'S', 'IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT', 'S', 'IDENT'],
+		'warnings': null
+	}
+});
