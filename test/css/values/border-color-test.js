668596a03781ca62302392e6da808961c67ff9d8
Tyler Akins <fidian@rumkin.com>
2012-04-03 14:46:24 -0500

    Initial commit

---
+++ test/css/values/border-color-test.js
@@ -0,0 +1,59 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('border-color', {
+	'blue': {
+		'tokens': ['IDENT'],
+		'toString': 'blue',
+		'unparsed': [],
+		'warnings': []
+	},
+	'blue red green idaho': {
+		'tokens': ['IDENT', 'S', 'IDENT', 'S', 'IDENT', 'S', 'IDENT'],
+		'toString': 'blue red green',
+		'unparsed': ['IDENT'],
+		'warnings': []
+	},
+	'blue, red, yellow': {
+		'tokens': ['IDENT', 'OPERATOR', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT'],
+		'toString': 'blue',
+		'unparsed': ['OPERATOR', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT'],
+		'warnings': []
+	},
+	'green transparent yellow blue red': {
+		'tokens': ['IDENT', 'S', 'IDENT', 'S', 'IDENT', 'S', 'IDENT', 'S', 'IDENT'],
+		'toString': 'green transparent yellow blue',
+		'unparsed': ['IDENT'],
+		'warnings': []
+	},
+	'transparent blue': {
+		'tokens': ['IDENT', 'S', 'IDENT'],
+		'toString': 'transparent blue',
+		'unparsed': [],
+		'warnings': []
+	},
+	'transparent': {
+		'tokens': ['IDENT'],
+		'toString': 'transparent',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2']
+	},
+	'green inherit blue red': {
+		'tokens': ['IDENT', 'S', 'IDENT', 'S', 'IDENT', 'S', 'IDENT'],
+		'toString': 'green inherit blue red',
+		'unparsed': [],
+		'warnings': ['inherit_not_allowed']
+	},
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': 'inherit',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2']
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
