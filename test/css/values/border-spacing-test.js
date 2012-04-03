d01c67cf139ff5a885b808abbf7f721eb2f6679b
Michael Plaxico <michael.plaxico@bestbuy.com>
2012-04-03 20:59:39 +0000

    Border Spacing test.

---
+++ test/css/values/border-spacing-test.js
@@ -0,0 +1,23 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('border-spacing', {
+	'10px': {
+		'tokens': ['UNIT'],
+		'toString': '10px',
+		'unparsed': [],
+		'warnings': []
+	},
+	'10px 10px': {
+		'tokens': ['UNIT', 'S', 'UNIT'],
+		'toString': '10px 10px',
+		'unparsed': [],
+		'warnings': []
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
