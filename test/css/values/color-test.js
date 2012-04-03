b9a886057bbaae55c4419d775d56d5e5df77371f
Tyler Akins <fidian@rumkin.com>
2012-04-03 16:36:01 -0500

    Initial commit

---
+++ test/css/values/color-test.js
@@ -0,0 +1,72 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('color', {
+	'inactiveborder': {
+		'tokens': ['IDENT'],
+		'toString': 'inactiveborder',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2', 'deprecated_css_v3_use_appearance']
+	},
+	'#fed': {
+		'tokens': ['HASH'],
+		'toString': '#fed',
+		'unparsed': [],
+		'warnings': []
+	},
+	'#f0f8ee': {
+		'tokens': ['HASH'],
+		'toString': '#f0f8ee',
+		'unparsed': [],
+		'warnings': []
+	},
+	'yellow': {
+		'tokens': ['IDENT'],
+		'toString': 'yellow',
+		'unparsed': [],
+		'warnings': []
+	},
+	'rgb(1, 2, 3)': {
+		'tokens': ['FUNCTION', 'UNIT', 'OPERATOR', 'S', 'UNIT', 'OPERATOR', 'S', 'UNIT', 'PAREN_CLOSE'],
+		'toString': 'rgb(1, 2, 3)',
+		'unparsed': [],
+		'warnings': []
+	},
+	'purple': {
+		'tokens': ['IDENT'],
+		'toString': 'purple',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2']
+	},
+	'orange': {
+		'tokens': ['IDENT'],
+		'toString': 'orange',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2.1']
+	},
+	'transparent blah blah': {
+		'tokens': ['IDENT', 'S', 'IDENT', 'S', 'IDENT'],
+		'toString': 'transparent',
+		'unparsed': ['IDENT', 'S', 'IDENT'],
+		'warnings': ['minimum_css_version_3']
+	},
+	'burlywood': {
+		'tokens': ['IDENT'],
+		'toString': 'burlywood',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_3']
+	},
+	// No minimum css version here since it's part of CSS1
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': 'inherit',
+		'unparsed': [],
+		'warnings': []
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
