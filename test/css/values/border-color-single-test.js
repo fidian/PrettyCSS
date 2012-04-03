f699cc22fab3d1e46e41f3c23b932ba40bbee344
Michael Plaxico <michael.plaxico@bestbuy.com>
2012-04-03 19:45:18 +0000

    Test for border-color-single.js.

---
+++ test/css/values/border-color-single-test.js
@@ -0,0 +1,29 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('border-color-single', {
+	'#fff': {
+		'tokens': ['HASH'],
+		'toString': '#fff',
+		'unparsed': [],
+		'warnings': []
+	},
+	'blue': {
+		'tokens': ['IDENT'],
+		'toString': 'blue',		
+		'unparsed': [],
+		'warnings': []
+	},
+	'transparent': {
+		'tokens': ['IDENT'],
+		'toString': 'transparent',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2.1']
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
