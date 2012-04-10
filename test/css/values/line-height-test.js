e0a4d00c3e567398966c5037ce3795351c8ff9f5
Michael Plaxico <michael.plaxico@bestbuy.com>
2012-04-10 16:50:09 +0000

    Line height test.

---
+++ test/css/values/line-height-test.js
@@ -0,0 +1,35 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('line-height', {
+	'normal': {
+		'tokens': ['IDENT'],
+		'toString': 'normal',
+		'unparsed': [],
+		'warnings': []
+	},
+	'10': {
+		'tokens': ['UNIT'],
+		'toString': '10',
+		'unparsed': [],
+		'warnings': []
+	},
+	'50em': {
+		'tokens': ['UNIT'],
+		'toString': '50em',
+		'unparsed': [],
+		'warnings': []
+	},
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': 'inherit',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2']
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
