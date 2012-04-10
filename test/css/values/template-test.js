87cf812206e3827d6d9e4e82a0ab0a9852acc50c
Tyler Akins <fidian@rumkin.com>
2012-04-10 16:40:52 -0500

    Added test and fixed bugs in parsing templates

---
+++ test/css/values/template-test.js
@@ -0,0 +1,42 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('template', {
+	'"any valid string"': {
+		'tokens': ['STRING'],
+		'toString': '"any valid string"',
+		'unparsed': [],
+		'warnings': ['working_draft']
+	},
+	'"asdf"/1.5px "asd " / 2em': {
+		'tokens': ['STRING', 'OPERATOR', 'UNIT', 'S', 'STRING', 'S', 'OPERATOR', 'S', 'UNIT'],
+		'toString': '"asdf"/1.5px "asd "/2em',
+		'unparsed': [],
+		'warnings': ['working_draft']
+	},
+	'"aa bb cc"/100px 3em 3em grover': {
+		'tokens': ['STRING', 'OPERATOR', 'UNIT', 'S', 'UNIT', 'S', 'UNIT', 'S', 'IDENT'],
+		'toString': '"aa bb cc"/100px 3em 3em',
+		'unparsed': ['IDENT'],
+		'warnings': ['working_draft']
+	},
+	'"valid" inherit': {
+		'tokens': ['STRING', 'S', 'IDENT'],
+		'toString': '"valid" inherit',
+		'unparsed': [],
+		'warnings': ['working_draft', 'inherit_not_allowed']
+	},
+	// inherit isn't a valid template
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
