aa7a1f33ad491e8b295424aa96c67789f6b444ee
Tyler Akins <fidian@rumkin.com>
2012-04-10 11:22:03 -0500

    Initial commit and bugfix

---
+++ test/css/values/font-variant-css21-test.js
@@ -0,0 +1,29 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('font-variant-css21', {
+	'normal': {
+		'tokens': ['IDENT'],
+		'toString': 'normal',
+		'unparsed': [],
+		'warnings': []
+	},
+	'small-caps normal': {
+		'tokens': ['IDENT', 'S', 'IDENT'],
+		'toString': 'small-caps',
+		'unparsed': ['IDENT'],
+		'warnings': []
+	},
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': 'inherit',
+		'unparsed': [],
+		'warnings': ['minimum_css_version_2']
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
