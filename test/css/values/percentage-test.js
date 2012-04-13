2a5d852ee56e4cb323ac4f25d2a584d7bfa31b7c
Tyler Akins <fidian@rumkin.com>
2012-04-13 10:45:38 -0500

    More flexible number parsing + new test

---
+++ test/css/values/percentage-test.js
@@ -0,0 +1,54 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('percentage', {
+	'37%': {
+		'tokens': ['UNIT'],
+		'toString': '37%',
+		'unparsed': [],
+		'warnings': []
+	},
+	'13.%': {
+		'tokens': ['UNIT'],
+		'toString': '13%',
+		'unparsed': [],
+		'warnings': ['integer_value_required']
+	},
+	'13.12%': {
+		'tokens': ['UNIT'],
+		'toString': '13%',
+		'unparsed': [],
+		'warnings': ['integer_value_required']
+	},
+	'.12%': {
+		'tokens': ['UNIT'],
+		'toString': '0%',
+		'unparsed': [],
+		'warnings': ['integer_value_required']
+	},
+	'-12%': {
+		'tokens': ['UNIT'],
+		'toString': '-12%',
+		'unparsed': [],
+		'warnings': []
+	},
+	'110% inherit': {
+		'tokens': ['UNIT', 'S', 'IDENT'],
+		'toString': '110%',
+		'unparsed': ['IDENT'],
+		'warnings': []
+	},
+	// Inherit is not allowed for the basic type
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
