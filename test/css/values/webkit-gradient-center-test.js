3e87ca45e4724e6db7c5502cdfe28eeaad1752e1
Tyler Akins <fidian@rumkin.com>
2012-05-22 13:45:18 -0500

    Tests for webkit-gradient and bugfix

---
+++ test/css/values/webkit-gradient-center-test.js
@@ -0,0 +1,41 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('webkit-gradient-center', {
+	'1em 1em': {
+		'tokens': ['UNIT', 'S', 'UNIT'],
+		'toString': '1em 1em',
+		'unparsed': [],
+		'warnings': []
+	},
+	'1em': {
+		'tokens': ['UNIT'],
+		'toString': null,
+		'unparsed': ['UNIT'],
+		'warnings': null
+	},
+	'1em yellow': {
+		'tokens': ['UNIT', 'S', 'IDENT'],
+		'toString': null,
+		'unparsed': ['UNIT', 'S', 'IDENT'],
+		'warnings': null
+	},
+	'top left 1em': {
+		'tokens': ['IDENT', 'S', 'IDENT', 'S', 'UNIT'],
+		'toString': 'top left',
+		'unparsed': ['UNIT'],
+		'warnings': []
+	},
+	'center': {
+		'tokens': ['IDENT'],
+		'toString': 'center',
+		'unparsed': [],
+		'warnings': []
+	},
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
---
+++ test/css/values/webkit-gradient-test.js
@@ -0,0 +1,39 @@
+"use strict";
+var util = require('./util');
+
+exports.batch = util.makeVows('webkit-gradient', {
+	'-webkit-gradient(linear, left, top right, from(blue), to(red))': {
+		'tokens': ['IDENT', 'CHAR', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'S', 'IDENT', 'OPERATOR', 'S', 'FUNCTION', 'IDENT', 'PAREN_CLOSE', 'OPERATOR', 'S', 'FUNCTION', 'IDENT', 'PAREN_CLOSE', 'PAREN_CLOSE'],
+		'toString': '-webkit-gradient(linear, left, top right, from(blue), to(red))',
+		'unparsed': [],
+		'warnings': []
+	},
+	'-webkit-gradient(radial, 40% 40%, 1em, center, 20px, color-stop(30%, #FFFF88), color-stop(0.4, green))': {
+		'tokens': [
+			'IDENT', 'CHAR',
+			'IDENT', 'OPERATOR',
+			'S', 'UNIT', 'S', 'UNIT', 'OPERATOR',
+			'S', 'UNIT', 'OPERATOR',
+			'S', 'IDENT', 'OPERATOR',
+			'S', 'UNIT', 'OPERATOR',
+			'S', 'FUNCTION', 'UNIT', 'OPERATOR', 'S', 'HASH', 'PAREN_CLOSE', 'OPERATOR',
+			'S', 'FUNCTION', 'UNIT', 'OPERATOR', 'S', 'IDENT', 'PAREN_CLOSE',
+			'PAREN_CLOSE'],
+		'toString': '-webkit-gradient(radial, 40% 40%, 1em, center, 20px, color-stop(30%, #FFFF88), color-stop(0.4, green))',
+		'unparsed': [],
+		'warnings': []
+	},
+	'-webkit-gradient(linear, left, top right, blue, red)': {
+		'tokens': ['IDENT', 'CHAR', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'PAREN_CLOSE'],
+		'toString': null,
+		'unparsed': ['IDENT', 'CHAR', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'OPERATOR', 'S', 'IDENT', 'PAREN_CLOSE'],
+		'warnings': null
+	},
+	// inherit is invalid
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
+});
