0f8f25bf1538f5eedf636d55ba4cb1a6ec9d9325
Tyler Akins <fidian@rumkin.com>
2017-09-07 07:40:43 -0500

    Spaces to tabs

--- test/css/values/text-shadow-single-test.js
+++ test/css/values/text-shadow-single-test.js
@@ -2,52 +2,52 @@
 var util = require('./util');
 
 exports.batch = util.makeVows('text-shadow-single', {
-    'none': {
-        'tokens': ['IDENT'],
-        'toString': 'none',
-        'unparsed': [],
-        'warnings': ['css-minimum:3']
-    },
-    '12px': {
-        'tokens': ['UNIT'],
-        'toString': null,
-        'unparsed': ['UNIT'],
-        'warnings': null
-    },
-    '12px 12px watermelon': {
-        'tokens': ['UNIT', 'S', 'UNIT', 'S', 'IDENT'],
-        'toString': '12px 12px',
-        'unparsed': ['IDENT'],
-        'warnings': ['css-minimum:3']
-    },
-    '12px 12px blue 12px': {
-        'tokens': ['UNIT', 'S', 'UNIT', 'S', 'IDENT', 'S', 'UNIT'],
-        'toString': '12px 12px blue',
-        'unparsed': ['UNIT'],
-        'warnings': ['css-minimum:3']
-    },
-    'blue 12px 1px 5px': {
-        'tokens': ['IDENT', 'S', 'UNIT', 'S', 'UNIT', 'S', 'UNIT'],
-        'toString': 'blue 12px 1px 5px',
-        'unparsed': [],
-        'warnings': ['css-minimum:3']
-    },
-    '12px 12px inherit': {
-        'tokens': ['UNIT', 'S', 'UNIT', 'S', 'IDENT'],
-        'toString': '12px 12px inherit',
-        'unparsed': [],
-        'warnings': ['css-minimum:3', 'inherit-not-allowed']
-    },
-    'inherit': {
-        'tokens': ['IDENT'],
-        'toString': 'inherit',
-        'unparsed': [],
-        'warnings': ['css-minimum:3']
-    },
-    'invalidValue': {
-        'tokens': ['IDENT'],
-        'toString': null,
-        'unparsed': ['IDENT'],
-        'warnings': null
-    }
+	'none': {
+		'tokens': ['IDENT'],
+		'toString': 'none',
+		'unparsed': [],
+		'warnings': ['css-minimum:3']
+	},
+	'12px': {
+		'tokens': ['UNIT'],
+		'toString': null,
+		'unparsed': ['UNIT'],
+		'warnings': null
+	},
+	'12px 12px watermelon': {
+		'tokens': ['UNIT', 'S', 'UNIT', 'S', 'IDENT'],
+		'toString': '12px 12px',
+		'unparsed': ['IDENT'],
+		'warnings': ['css-minimum:3']
+	},
+	'12px 12px blue 12px': {
+		'tokens': ['UNIT', 'S', 'UNIT', 'S', 'IDENT', 'S', 'UNIT'],
+		'toString': '12px 12px blue',
+		'unparsed': ['UNIT'],
+		'warnings': ['css-minimum:3']
+	},
+	'blue 12px 1px 5px': {
+		'tokens': ['IDENT', 'S', 'UNIT', 'S', 'UNIT', 'S', 'UNIT'],
+		'toString': 'blue 12px 1px 5px',
+		'unparsed': [],
+		'warnings': ['css-minimum:3']
+	},
+	'12px 12px inherit': {
+		'tokens': ['UNIT', 'S', 'UNIT', 'S', 'IDENT'],
+		'toString': '12px 12px inherit',
+		'unparsed': [],
+		'warnings': ['css-minimum:3', 'inherit-not-allowed']
+	},
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': 'inherit',
+		'unparsed': [],
+		'warnings': ['css-minimum:3']
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
 });
--- test/css/values/text-shadow-test.js
+++ test/css/values/text-shadow-test.js
@@ -2,28 +2,28 @@
 var util = require('./util');
 
 exports.batch = util.makeVows('text-shadow', {
-    'blue 12px 1px 5px, blue 12px 1px 5px': {
-        'tokens': ['IDENT', 'S', 'UNIT', 'S', 'UNIT', 'S', 'UNIT', 'OPERATOR', 'S', 'IDENT', 'S', 'UNIT', 'S', 'UNIT', 'S', 'UNIT'],
-        'toString': 'blue 12px 1px 5px, blue 12px 1px 5px',
-        'unparsed': [],
-        'warnings': ['css-minimum:3']
-    },
-    'none': {
-        'tokens': ['IDENT'],
-        'toString': 'none',
-        'unparsed': [],
-        'warnings': ['css-minimum:3']
-    },
-    'inherit': {
-        'tokens': ['IDENT'],
-        'toString': 'inherit',
-        'unparsed': [],
-        'warnings': ['css-minimum:3']
-    },
-    'invalidValue': {
-        'tokens': ['IDENT'],
-        'toString': null,
-        'unparsed': ['IDENT'],
-        'warnings': null
-    }
+	'blue 12px 1px 5px, blue 12px 1px 5px': {
+		'tokens': ['IDENT', 'S', 'UNIT', 'S', 'UNIT', 'S', 'UNIT', 'OPERATOR', 'S', 'IDENT', 'S', 'UNIT', 'S', 'UNIT', 'S', 'UNIT'],
+		'toString': 'blue 12px 1px 5px, blue 12px 1px 5px',
+		'unparsed': [],
+		'warnings': ['css-minimum:3']
+	},
+	'none': {
+		'tokens': ['IDENT'],
+		'toString': 'none',
+		'unparsed': [],
+		'warnings': ['css-minimum:3']
+	},
+	'inherit': {
+		'tokens': ['IDENT'],
+		'toString': 'inherit',
+		'unparsed': [],
+		'warnings': ['css-minimum:3']
+	},
+	'invalidValue': {
+		'tokens': ['IDENT'],
+		'toString': null,
+		'unparsed': ['IDENT'],
+		'warnings': null
+	}
 });
